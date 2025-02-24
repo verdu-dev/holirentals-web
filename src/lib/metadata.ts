d42bd6ec1c8e4c61dab527fd11b7b4d04b82c2a5
verdu-dev <verdukactus@gmail.com>
2025-02-24 20:05:20 +0100

    inital

---
+++ src/lib/metadata.ts
@@ -0,0 +1,13 @@
+interface Metadata {
+  title: string;
+  description: string;
+  image: string;
+  name: string;
+}
+
+export const metadata: Metadata = {
+  title: "Holirentals™ | Gestión de alquiler vacacional",
+  description: "Holirentals™ ofrece soluciones de gestión de alquiler de vacaciones de alta calidad, con un enfoque en la seguridad y la adaptabilidad a las normativas actuales.",
+  image: "card.jpg",
+  name: "Holirentals",
+}
